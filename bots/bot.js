fab31626649ce6680e752de7e1101534fd4cf077
awarn <anton@rebelandbird.com>
2017-02-18 16:35:56 +0100

    Undercut competitors on a schedule

--- bots/bot.js
+++ bots/bot.js
@@ -18,9 +18,9 @@ const request = require('request-promise');
 const lbc = new LBCClient(process.env.LOCALBITCOIN_KEY, process.env.LOCALBITCOIN_SECRET);
 
 // Timing
-let checkupSched = later.parse.text('every 5 seconds');
+let checkupSched = later.parse.text('every 10 seconds');
 
-let dontGoUnder = 8200;
+let dontGoUnder = 9000;
 let undercutAmount = 10;
 let paymentMethod = `SWISH`;
 let minimumMaxAmountAvailable = 10000;
@@ -48,9 +48,23 @@ async function getCountryCodes() {
 	return apiCall(method);
 }
 
-// Get listed ads
-async function getAds() {
+async function updateAd(adId, params) {
+	let method = 'ad';
+	return apiCall(method, adId, params);
+}
+
+// Get listed buy ads
+async function getBuyAds() {
 	let method = 'buy-bitcoins-online';
+	let params = {
+		path: 'SEK'
+	};
+	return apiCall(method, null, params);
+}
+
+// Get listed sell ads
+async function getSellAds() {
+	let method = 'sell-bitcoins-online';
 	let params = {
 		path: 'SEK'
 	};
@@ -85,13 +99,8 @@ function checkup() {
 			});
 
 			// Get the current SEK ads.
-			let allAds = await getAds();
+			let allAds = await getSellAds();
 			let adList = allAds.ad_list;
-
-			// Get our own ad(s).
-			let userSEKAdList = adList.filter((ad) => {
-				return userAdIds.includes(ad.data.ad_id);
-			});
 
 			// Filter out our own ad.
 			adList = adList.filter((ad) => {
@@ -104,6 +113,10 @@ function checkup() {
 			// Filter out ads not competing in trade bulk.
 			adList = adList.filter((ad) => {
 				return ad.data.max_amount_available > minimumMaxAmountAvailable;
+			});
+			// Filter out ads below or lowest threshold.
+			adList = adList.filter((ad) => {
+				return ad.data.temp_price - undercutAmount > dontGoUnder;
 			});
 
 			// If we are in the top three, only compete with those
@@ -123,16 +136,67 @@ function checkup() {
 				});
 			}
 
-			console.log("user ads");
-			for (var i = adList.length - 1; i >= 0; i--) {
-				let data = adList[i].data
-				console.log(data.ad_id, data.profile.username);
-			}
+			// Check that we have any ads left.
+			if (adList.length > 0) {
 
-			console.log("user ads");
-			for (var i = userAdList.length - 1; i >= 0; i--) {
-				let data = userAdList[i].data
-				console.log(data.ad_id, data.profile.username);
+				// Sort the remaining ads by temp_price.
+				adList.sort((a, b) => {
+					if (a.data.temp_price < b.data.temp_price) {
+						return -1;
+					}
+					else if (a.data.temp_price > b.data.temp_price) {
+						return 1;
+					}
+					else {
+						return 0;
+					}
+				});
+
+				for (var i = adList.length - 1; i >= 0; i--) {
+					let data = adList[i].data
+					console.log(data.ad_id, data.profile.username, data.temp_price);
+				}
+
+				// Get the lowest price of the remaining.
+				let targetPrice = adList[0].data.temp_price;
+				console.log("target price:", targetPrice);
+
+				// Undercut them
+				for (var i = userAdList.length - 1; i >= 0; i--) {
+					let data = userAdList[i].data
+
+					console.log("our price:", data.temp_price);
+
+					if (targetPrice < data.temp_price) {
+						let params = {
+							bank_name: data.bank_name,
+							min_amount: data.min_amount,
+							require_trusted_by_advertiser: data.require_trusted_by_advertiser,
+							track_max_amount: data.track_max_amount,
+							lat: data.lat,
+							price_equation: data.price_equation, // Changed below
+							city: data.city,
+							location_string: data.location_string,
+							countrycode: data.countrycode,
+							currency: data.currency,
+							max_amount: data.max_amount,
+							lon: data.lon,
+							sms_verification_required: data.sms_verification_required,
+							opening_hours: data.opening_hours,
+							msg: data.msg,
+							require_identification: data.require_identification,
+							'details-phone_number': data.account_details.phone_number
+						}
+
+						// Set our new price
+						params.price_equation = targetPrice - undercutAmount;
+
+						let result = await updateAd(data.ad_id, params);
+						console.log(`Adjusted ad price: ${data.temp_price} => ${targetPrice - undercutAmount}`);
+					} else {
+						console.log(`Currently below or at target price.`);
+					}
+				}
 			}
 
 		} catch(e) {
@@ -142,7 +206,6 @@ function checkup() {
 	})();
 }
 
-checkup();
-
 // Run it all
-// let checkupTimer = later.setInterval(checkup, checkupSched);
+let checkupTimer = later.setInterval(checkup, checkupSched);
+
